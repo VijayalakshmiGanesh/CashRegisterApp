00bdc5acf49addb0daad3a799e8d78331ba4597e
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2022-08-25 22:43:32 +0530

    handled all the cases

--- cashRegister.js
+++ cashRegister.js
@@ -3,36 +3,37 @@ var cashGiven = document.querySelector("#cashGiven");
 var check = document.querySelector("#submit")
 var notes = document.querySelectorAll(".no-of-notes");
 var message = document.querySelector("#msg");
+var resetBtn = document.querySelector("#reset")
 
 check.addEventListener("click", clickHandler);
-const money = [2000, 500, 200, 100, 50, 10, 5, 1];
+resetBtn.addEventListener("click", () => window.location.reload());
+const money = [2000, 500, 200, 100, 50, 10, 5, 2, 1];
 
 
 function clickHandler() {
 
-    if (!billAmount.value || !cashGiven.value) {
+    if (billAmount.value == '' || cashGiven.value == '') {
         alert("Enter all neccessary values")
         return
-    } else if (billAmount.value <= 0 || cashGiven.value <= 0) {
+    } else if (billAmount.value <= 0 || cashGiven.value < 0) {
         alert("Enter proper values")
         return
     }
     message.style.display = "none";
 
-    if (billAmount.value > 0) {
-        if (cashGiven.value > billAmount.value) {
-            let amountToBeReturned = cashGiven.value - billAmount.value;
-            calculate(amountToBeReturned);
-        } else if (cashGiven.value == billAmount.value) {
-            showMessage("No Return change required ");
-        }
-        else {
-            showMessage("Ready to wash the plates ...? ");
-        }
-    } else {
-        showMessage("Invalid amount");
-    }
+    billAmount = billAmount.value;
+    cashGiven = cashGiven.value;
+    var amountToBeReturned = cashGiven - billAmount;
 
+    console.log(billAmount, cashGiven, amountToBeReturned)
+    if (amountToBeReturned == 0) {
+        showMessage("No Return change required ");
+    } else if (amountToBeReturned < 0) {
+        showMessage("Ready to wash the plates ...? ");
+    } else {
+        showMessage("Amount to be returned is ₹" + amountToBeReturned)
+        calculate(amountToBeReturned);
+    }
 
 }
 
